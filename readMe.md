9ad2fbfb3a716728392eb71a15e4c890e9b5d6b6
shwethaa-rajesh <shwethaa_r@mckinsey.com>
2022-02-23 10:28:14 +0530

    feat:add airportDetails.js

---
+++ readMe.md
@@ -0,0 +1,7 @@
+## **AIRPORT INFORMATION MODULE**
+Takes an Airport Code and returns an Airport's
+1. Name
+2. City 
+3. Country 
+
+as an object. 
---
+++ src/airportDetails.js
@@ -0,0 +1,22 @@
+const airports = require('../resources/airportsInfo.json');
+
+const getAirportInfo = (airportCode) => {
+  if (typeof airportCode !== 'string' || airportCode.length !== 3) throw Error('The code is not a proper airport code');
+  let foundFlag = 0;
+  let airportInformation;
+  airports.forEach((airport) => {
+    if (airport.code.toLowerCase() === airportCode.toLowerCase()) {
+      foundFlag = 1;
+      airportInformation = {
+        Name: airport.name,
+        City: airport.city,
+        Country: airport.country,
+      };
+    }
+  });
+  if (foundFlag === 0) {
+    return 'No aiport found';
+  }
+  return airportInformation;
+};
+module.exports = { getAirportInfo };
