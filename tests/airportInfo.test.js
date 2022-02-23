ca4d305fa82da33cd4d633e6fe28235649c911dd
vri <Vriti_Satija@mckinsey.com>
2022-02-24 01:41:59 +0530

    feat: add getInfo test cases

---
+++ tests/airportInfo.test.js
@@ -0,0 +1,23 @@
+const getInfo = require('../src/airportInfo');
+
+describe('getInfo function', () => {
+  it('should give the object containing airport country,city and name', () => {
+    const airport = getInfo('ZXE');
+    const airportData = {
+      country: 'United Kingdom',
+      city: 'Edinburgh',
+      name: 'Waverley station',
+    };
+    expect(airport).toStrictEqual(airportData);
+  });
+  it('should give error when airport does not exist', () => {
+    const airport = getInfo('VRI');
+    expect(airport).toBe('Airport does not exists');
+  });
+  it('should give error when airport code has invalid format', () => {
+    const airport = getInfo([6, 9, 8]);
+    expect(airport).toBe('Airport does not exists');
+  });
+});
+// node ./src/airportInfo.js
+// ./node_modules/.bin/jest.cmd ./tests/airportInfo.test.js
