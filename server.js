7b2b45961ca1dade0d6c0e3574c71936f69c570f
Sara-Jain <Sara_Jain@mckinsey.com>
2022-02-23 19:06:13 +0530

    refactor: change variables

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const port = 3000;
 const airportDetails = require("airportmodule");
 
 const requestHandler = (req, res) => {
-   // query params extract
-   const queryObject = url.parse(req.url, true).query;
-   // filter response
-   res.end(JSON.stringify(airportDetails.findAirportDetails(queryObject.airportcode)));
+  // query params extractation
+  const queryObject = url.parse(req.url, true).query;
+  // filter response
+  // console.log("The query object is");
+  // console.log(queryObject);
+  // console.log(queryObject.airportcode);
+  const aboutAirportObject = airportDetails.findAirportDetails(queryObject.airportcode);
+  res.end(JSON.stringify(aboutAirportObject));
 };
-
-
 const server = http.createServer(requestHandler);
 
 server.listen(port, host, () => {
