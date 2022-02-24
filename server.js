9ab9b289aaa1d3fa0efca612b1c83f3439e8e093
AishMckinsey <99251793+AishMckinsey@users.noreply.github.com>
2022-02-24 11:41:15 +0530

    feat: add server.js

--- server.js
+++ server.js
@@ -1,44 +1,44 @@
+// Request url
+// http://localhost:3000/airport-details?airport-code=DEL
+
+const {airportDataProvider} = require('../src/airportDataProvider');
+
 const http = require("http");
+const url = require('url');
+
 const host = "localhost";
 const port = 3000;
 
 const requestHandler = (req, res) => {
-  res.end("Hello");
-};
-
-/*
-const requestHandler = (req, res) => {
   res.setHeader("Content-Type", "application/json");
+
+  const queryObject = url.parse(req.url, true).query;
+  
   res.writeHead(200);
-  res.end(`{"message": "This is a JSON response"}`);
+  const airportCode = queryObject['airport-code'];
+  res.end(JSON.stringify(airportDataProvider(airportCode)));
+
 };
 
-
-const requestHandler = (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  switch (req.url) {
-    case "/":
-      res.writeHead(200);
-      res.end("Its working!");
-      break;
-    case "/ping":
-      res.writeHead(200);
-      res.end("Pong");
-      break;
-    case "/hello-json":
-      res.writeHead(200);
-      res.end(
-        JSON.stringify({
-          message: "Hello this is json response",
-        })
-      );
-      break;
-  }
-};
-*/
 
 const server = http.createServer(requestHandler);
 
 server.listen(port, host, () => {
   console.log(`Server started. Visit http://${host}:${port}`);
 });
+
+
+/*
+switch (req.url) {
+    case "/":
+      res.writeHead(200);
+      res.end("Its working!");
+      break;
+    case "/airport-details?airport-code=DEL":
+      res.writeHead(200);
+      //console.log(queryObject['airport-code']);
+      const airportCode = queryObject['airport-code'];
+      res.end(JSON.stringify(airportDataProvider(airportCode)));
+      break;
+ }
+*/
