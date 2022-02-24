9f39078658640a727a843ca17158a6a590769e22
Twissa Modi <Twissa_Modi@mckinsey.com>
2022-02-24 12:18:46 +0530

    fix: modify content-type

--- servers/server.js
+++ servers/server.js
@@ -7,14 +7,16 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   const queries = url.parse(req.url, true).query;
-  res.setHeader('Content-Type', 'text/plain');
+  
   if ('airport-code' in queries) {
     try {
       const airportDetails = airport.getDetails(queries['airport-code']);
+      res.setHeader('Content-Type', 'application/json');
       res.writeHead(200);
       res.end(JSON.stringify(airportDetails, null, '\t'));
     } catch (err) {
       res.writeHead(400);
+      res.setHeader('Content-Type', 'text/plain');
       res.end(err.message);
     }
   }
